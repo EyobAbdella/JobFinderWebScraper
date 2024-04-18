f959a49bdfdc095d55f1e5c165d26ccd9d71c98e
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-04-18 15:40:56 -0400

    Refactor the code

--- db.py
+++ db.py
@@ -9,44 +9,65 @@ db_user = os.environ.get("DB_USER")
 db_password = os.environ.get("DB_PASSWORD")
 
 
-db = mysql.connector.connect(
-    host=db_host, user=db_user, password=db_password, database="job_finder"
-)
-
-cursor = db.cursor()
-
-
 class Database:
     def __init__(self, jobs):
         self.jobs = jobs
+        self.db = mysql.connector.connect(
+                    host=db_host, user=db_user, password=db_password, database="job_finder")
+        self.cursor = self.db.cursor()
 
-    def job_exits(job_id):
-        cursor.execute("SELECT COUNT(*) FROM jobs WHERE id = %s", (job_id,))
-        count = cursor.fetchone()[0]
-        return count > 0
+
+    def setup_database_and_table(self):
+        self.cursor.execute("CREATE DATABASE IF NOT EXISTS job_finder")
+        self.cursor.execute("USE job_finder")
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS jobs (
+                id INT NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                salary_offer VARCHAR(255) NOT NULL,
+                experience_level VARCHAR(255) NOT NULL,
+                years_of_experience VARCHAR(255) NOT NULL,
+                date_posted DATE NOT NULL,
+                deadline_date DATE NOT NULL,
+                job_location VARCHAR(255) NOT NULL,
+                job_type VARCHAR(255),
+                job_category VARCHAR(255),
+                application_process TEXT NOT NULL,
+                PRIMARY KEY (id)
+            )
+            """
+        )
+        self.db.commit()
+
 
     def insert_jobs(self):
         for job in self.jobs:
-            cursor.execute(
+            self.cursor.execute(
                 """
                 INSERT INTO jobs (id, title, salary_offer, experience_level, years_of_experience, date_posted, deadline_date, job_location, job_type, job_category, application_process)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)           
-                           """
-            ), (
-                job.id,
-                job.title,
-                job.salary_offer,
-                job.experience_level,
-                job.years_of_experience,
-                job.date_posted,
-                job.deadline_date,
-                job.job_location,
-                job.job_type,
-                job.job_category,
-                job.application_process,
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)           
+                """,
+                (
+                    job.get("id"),
+                    job.get("title"),
+                    job.get("salary_offer"),
+                    job.get("experience_level"),
+                    job.get("years_of_experience"),
+                    job.get("date_posted"),
+                    job.get("deadline_date"),
+                    job.get("job_location"),
+                    job.get("job_type"),
+                    job.get("job_category"),
+                    job.get("application_process"),
+                ),
             )
-            db.commit()
+            self.db.commit()
 
-    def run(self): ...
+    def run(self):
+        # self.setup_database_and_table()
+        self.insert_jobs()
 
-    db.close()
+    def close(self):
+        self.cursor.close()
+        self.db.close()
--- main.py
+++ main.py
@@ -1,125 +1,158 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from db import Database
 import requests
+import logging
+
+logging.basicConfig(
+    filename='app.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+    )
+logger = logging.getLogger(__name__)
 
 
-def job_listing(page_number):
+def filter_jobs(page_number):
     url = f"https://www.ethiopianreporterjobs.com/jobs-in-ethiopia/page/{page_number}"
-    page = requests.get(url)
+    try:
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        job_container = soup.find(class_="-wrap")
+        time_units = ["day", "days", "week", "weeks", "month", "months"]
+        jobs = job_container.find_all(class_="loop-item-wrap list")
+        job_links = []
+        for job in jobs:
+            job_date = job.find(class_="job-date-ago").text.strip().lower()
+            if any(unit in job_date for unit in time_units):
+                break
+            job_link = job.find(class_="loop-item-title").find("a")["href"]
+            job_links.append(job_link)
+        return job_links
+    except Exception as e:
+        logger.error(f"Failed to fetch job links for page {page_number}: {e}")
+        return []
 
-    soup = BeautifulSoup(page.text, "html.parser")
-
-    job_container = soup.find(class_="-wrap")
-
-    time_units = ["day", "days", "week", "weeks", "month", "months"]
-
-    jobs = job_container.find_all(class_="loop-item-wrap list")
-    job_links = []
-    for job in jobs:
-        job_date = job.find(class_="job-date-ago").text.strip().lower()
-        if any(unit in job_date for unit in time_units):
-            break
-        job_link = job.find(class_="loop-item-title").find("a")["href"]
-        job_links.append(job_link)
-    return job_links
-
-
-def get_job():
+def get_job_links():
     url = "https://www.ethiopianreporterjobs.com/jobs-in-ethiopia/"
-    request = requests.get(url)
-    soup = BeautifulSoup(request.text, "html.parser")
-    pages = soup.find_all(class_="page-numbers")
-    page_number = int(pages[len(pages) - 2].text)
-    job_links = []
-    for page in range(4, int(page_number / 2)):
-        job = job_listing(page)
-        job_links.extend(job)
-        if len(job) < 10:
-            break
-    return job_links
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        pages = soup.find_all(class_="page-numbers")
+        page_number = int(pages[len(pages) - 2].text)
+        job_links = []
+        for page in range(2, int(page_number / 2)):
+            job = filter_jobs(page)
+            job_links.extend(job)
+            if len(job) < 10:
+                break
+        return job_links
+    except Exception as e:
+        logger.error(f"Failed to fetch job links: {e}")
+        return []
 
 
-def clean_code(code):
-    unwanted_class_names = [
-        "addtoany_share_save_container addtoany_content addtoany_content_bottom",
-        "code-block code-block-2",
-        "pvc_clear",
-        "pvc_stats all pvc_load_by_ajax_update",
-        "pvc_clear",
-    ]
-    soup = BeautifulSoup(code, "html.parser")
+def clean_text(text):
+    try:
+        unwanted_class_names = [
+            "addtoany_share_save_container addtoany_content addtoany_content_bottom",
+            "code-block code-block-2",
+            "pvc_clear",
+            "pvc_stats all pvc_load_by_ajax_update",
+            "pvc_clear",
+        ]
+        soup = BeautifulSoup(text, "html.parser")
 
-    for unwanted_cn in unwanted_class_names:
-        unwanted_element = soup.find(class_=unwanted_cn)
-        unwanted_element.extract()
+        for unwanted_cn in unwanted_class_names:
+            unwanted_element = soup.find(class_=unwanted_cn)
+            unwanted_element.extract()
 
-    return str(soup)
-
+        return str(soup)
+    except Exception as e:
+        logger.error(f"Failed to clean text: {e}")
+        return text
 
 def get_id(url):
-    parsed_url = urlparse(url)
-    path = parsed_url.path
-    path_parts = path.split("/")
-    return int(path_parts[-2])
-
+    try:
+        parsed_url = urlparse(url)
+        path = parsed_url.path
+        path_parts = path.split("/")
+        return int(path_parts[-2])
+    except Exception as e:
+        logger.error(f"Failed to get job ID: {e}")
+        return None
 
 def job_detail(link):
-    # Check the job exists before creating it on the database by comparing the last job ID with the new one
-    detail = requests.get(link)
-    soup = BeautifulSoup(detail.text, "html.parser")
-    id = get_id(link)
-    job_title = soup.find(class_="page-title").text.replace("\n", "")
-    salary_offer = soup.find(class_="value-_noo_job_field_salary cf-select-value").text
-    experience_level = soup.find(
-        class_="value-_noo_job_field_experience_level cf-select-value"
-    ).text
-    years_of_experience = soup.find(
-        class_="value-_noo_job_field_year_experience cf-select-value"
-    ).text
-    date_posted = soup.find(
-        class_="value-_noo_job_field_date_posted cf-datepicker-value"
-    ).text
-    deadline_date = soup.find(
-        class_="value-_noo_job_field_dead_line cf-datepicker-value"
-    ).text
+    try:
+        response = requests.get(link)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        id = get_id(link)
+        job_title = soup.find(class_="page-title").text.replace("\n", "")
+        salary_offer = soup.find(class_="value-_noo_job_field_salary cf-select-value").text
+        experience_level = soup.find(
+            class_="value-_noo_job_field_experience_level cf-select-value"
+        ).text
+        years_of_experience = soup.find(
+            class_="value-_noo_job_field_year_experience cf-select-value"
+        ).text
+        date_posted = soup.find(
+            class_="value-_noo_job_field_date_posted cf-datepicker-value"
+        ).text
+        deadline_date = soup.find(
+            class_="value-_noo_job_field_dead_line cf-datepicker-value"
+        ).text
 
-    job_location = soup.find(class_="job-location").text.replace("\n", "")
+        job_location = soup.find(class_="job-location").text.replace("\n", "")
 
-    job_type = soup.find(class_="job-type").text
+        job_type = soup.find(class_="job-type").text
 
-    job_category = soup.find(class_="job-category").text.replace("\n", "")
+        job_category = soup.find(class_="job-category").text.replace("\n", "")
 
-    # Use Rich Text Editor to display
-    job_requirement_and_how_to_apply = soup.find(class_="map-style-2")
-    application_process = clean_code(
-        job_requirement_and_how_to_apply.prettify().replace("\n", "")
-    )
-    job = {
-        "id": id,
-        "title": job_title,
-        "salary_offer": salary_offer,
-        "experience_level": experience_level,
-        "years_of_experience": years_of_experience,
-        "date_posted": date_posted,
-        "deadline_date": deadline_date,
-        "job_location": job_location,
-        "job_type": job_type,
-        "job_category": job_category,
-        "application_process": application_process,
-    }
-    return job
-
-
-def insert_job(job): ...
-def job_exists(job_id): ...
-
+        job_requirement_and_how_to_apply = soup.find(class_="map-style-2")
+        application_process = clean_text(
+            job_requirement_and_how_to_apply.prettify().replace("\n", "")
+        )
+        job = {
+            "id": id,
+            "title": job_title,
+            "salary_offer": salary_offer,
+            "experience_level": experience_level,
+            "years_of_experience": years_of_experience,
+            "date_posted": date_posted,
+            "deadline_date": deadline_date,
+            "job_location": job_location,
+            "job_type": job_type,
+            "job_category": job_category,
+            "application_process": application_process,
+        }
+        return job
+    except Exception as e:
+        logger.error(f"Failed to get job detail: {e}")
+        return None
 
 def save_on_database():
-    jobs = get_job()
-    for job in jobs:
-        detail = job_detail(job)
-        print(detail)
-        break
+    try:
+        jobs = get_job_links()
+        data = []
+        for job in jobs:
+            detail = job_detail(job)
+            data.append(detail)
+        return data
+    except Exception as e:
+        logger.error(f"Failed to save jobs on database: {e}")
+        return []
 
 
-save_on_database()
+if __name__ == "__main__":
+    job_data = save_on_database()
+    if job_data:
+        database = Database(job_data)
+        database.run()
+        database.close()
+        logger.info("Data saved successfully.")
+    else:
+        logger.error("No job data to save.")
